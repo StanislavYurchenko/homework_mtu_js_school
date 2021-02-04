709e38bf042854f2e9013c784a47dbbbd7562016
Yurchenko Stanislav <yurchenko.stanislav@ukr.net>
2021-02-04 11:27:29 +0200

    delete new task for relocate

--- create_react_app_story_book_2021__02_03/.storybook/preview.js
+++
@@ -1,8 +0,0 @@
-import { addDecorator } from '@storybook/react'
-import { jsxDecorator } from 'storybook-addon-jsx'
-
-addDecorator(jsxDecorator);
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
